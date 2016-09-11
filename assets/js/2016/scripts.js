b8503fca6461ce62ca8d0aac19eecdccc3ccef6d
Ivan Dalmet <ivan@creativedata.fr>
2016-09-11 18:48:56 +0200

    feat: Add truncate text for speaker cards.

---
+++ assets/js/2016/scripts.js
@@ -0,0 +1,41 @@
+(function() {
+    'use strict';
+
+    /* ------------------------- *\
+        Truncate text
+    \* ------------------------- */
+    var truncateTextElements;
+
+    function initTruncateText() {
+      truncateTextElements = document.getElementsByClassName('js-truncate-text');
+
+      for (var i = 0; i < truncateTextElements.length; i++) {
+        var element = truncateTextElements[i];
+
+        if (element.clientHeight > 100) {
+          element.classList.add('truncate-text');
+          element.innerHTML = '<button class="truncate-text-toggle">Show more</button>' + element.innerHTML;
+        }
+
+        element.addEventListener('click', showTruncateText, false);
+      }
+    }
+
+    function showTruncateText() {
+      this.classList.add('open');
+      this.removeEventListener('click', showTruncateText, false);
+    }
+
+
+    /* ------------------------- *\
+        Init when DOM ready
+    \* ------------------------- */
+    document.addEventListener("DOMContentLoaded", function() {
+      initTruncateText();
+    });
+
+    /* ------------------------- *\
+        Public
+    \* ------------------------- */
+    return {};
+})();
