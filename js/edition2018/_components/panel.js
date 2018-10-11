b5efb4417e43f65aa8c9217ffade820650b8cfac
Ivan Dalmet <ivan@creativedata.fr>
2018-10-11 15:13:15 +0200

    feat: Programme 2018

--- js/edition2018/_components/panel.js
+++ js/edition2018/_components/panel.js
@@ -1,17 +1,49 @@
 (() => {
-  const openedClass = 'is-open';
+  const panelOpenedClass = 'is-open';
   const $openButtons = document.querySelectorAll('[data-panel-open]');
   const $closeButtons = document.querySelectorAll('[data-panel-close]');
   const $panels = document.querySelectorAll('.js-panel');
 
-  // Close all panels
-  const closeAll = () => {
-    [].forEach.call($panels, $panel => {
-      $panel.classList.remove(openedClass);
+  // Init
+  [].forEach.call($panels, $panel => {
+    $panel.addEventListener('click', event => event.stopPropagation());
+  });
+
+  const onClickOutside = () => {
+    closeAll();
+  };
+
+  const onKeyPress = (event) => {
+    const isEscape = ("key" in event && (event.key === "Escape" || event.key === "Esc")) || event.keyCode === 27;
+
+    if (isEscape) {
+      closeAll();
+    }
+  };
+
+  const openPanel = ($panel) => {
+    $panel.classList.add(panelOpenedClass);
+
+    setTimeout(() => {
+      document.addEventListener('click', onClickOutside);
+      document.addEventListener('keydown', onKeyPress);
     });
   };
 
-  // Open
+  const closePanel = ($panel) => {
+    $panel.classList.remove(panelOpenedClass);
+
+    document.removeEventListener('click', onClickOutside);
+    document.removeEventListener('keydown', onKeyPress);
+  };
+
+  const closeAll = () => {
+    [].forEach.call($panels, $panel => {
+      closePanel($panel);
+    });
+  };
+
+  // Open (toggle) buttons
   [].forEach.call($openButtons, $button => {
     const panelId = $button.getAttribute('data-panel-open');
 
@@ -22,20 +54,20 @@
         return;
       }
 
-      const isOpen = $panel.classList.contains(openedClass);
+      const isOpen = $panel.classList.contains(panelOpenedClass);
 
       event.preventDefault();
       closeAll();
 
       if (isOpen) {
-        $panel.classList.remove(openedClass);
+        closePanel($panel);
       } else {
-        $panel.classList.add(openedClass);
+        openPanel($panel);
       }
     });
   });
 
-  // Close
+  // Close buttons
   [].forEach.call($closeButtons, $button => {
     $button.addEventListener('click', (event) => {
       event.preventDefault();
